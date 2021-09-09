ea79969926fe88ea651bd8bf72db5dca58165eb7
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-09 22:55:53 +0500

    chore: prepared layout

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -8,6 +8,31 @@
       >
       </v-text-field>
     </v-form>
+    <v-card class="mb-5">
+      <v-list-item>
+        <v-list-item-content>
+          <v-checkbox
+              hide-details
+              class="shrink mr-2 mt-0"
+          >
+            <template v-slot:label>
+              <div>Task</div>
+            </template>
+          </v-checkbox>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn
+              color="error"
+              ripple
+          >
+            <v-icon color="white" left>
+              mdi-trash-can
+            </v-icon>
+            Delete
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-card>
   </v-container>
 </template>
 
