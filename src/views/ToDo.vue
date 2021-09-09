a0825d0b9c447f29758d84f8f3bfc3f6d54f6068
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-09 22:54:55 +0500

    chore: added form input for adding task

---
+++ src/views/ToDo.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-text-field
+          label="Введите название дела"
+          outlined
+          clearable
+      >
+      </v-text-field>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ToDo",
+
+}
+</script>
+
+<style scoped>
+
+</style>
