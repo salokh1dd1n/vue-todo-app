f7c98cea40afe4dacfbd8781f02aaaab07de113d
Salokhiddin <berdiyarovsalohiddin@gmail.com>
2021-09-11 22:04:41 +0500

    chore: created Filter.vue component

---
+++ src/components/todo/Filter.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-radio-group
+      row
+      class="float-end"
+      v-model="filter"
+  >
+    <v-radio
+        label="All"
+    ></v-radio>
+    <v-radio
+        label="Only completed"
+    ></v-radio>
+    <v-radio
+        label="Only in process"
+    ></v-radio>
+  </v-radio-group>
+</template>
+
+<script>
+export default {
+  name: "Filter",
+  data() {
+    return {
+      filter: "",
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
